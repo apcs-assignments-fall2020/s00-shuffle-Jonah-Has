7c2e5589588a14699866c976b2441d9d6775bd4f
Jonah-Has <23jhasegawa@athenian.org>
2021-01-11 07:59:47 +0000

    Wrote code for both shuffles

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,12 +2,35 @@ public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+
+        int len1 = arr.length / 2;
+        int len2 = arr.length - len1;
+
+        int[] shuffled = new int[arr.length];
+
+        for (int i = 0; i < len1; i++){
+            shuffled[2 * i] = arr[i];
+        }
+        for (int i = 0; i < len2; i++){
+            shuffled[(2 * i) + 1] = arr[len1 + i];
+        }
+
+        return shuffled;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
+
+        int len = arr.length;
+        int[] shuffled = arr;
+        for (int i = 0; i < len; i++){
+            int ran = (int) (Math.random() * len);
+            int temp = shuffled[i];
+            shuffled[i] = shuffled[ran];
+            shuffled[ran] = temp;
+        }
+        
+        return shuffled;
     }
 
 
